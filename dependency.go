e5d24273c82907d437ed55d47465e6720f40b596
kay <kay.ding@trantect.com>
2023-08-04 17:25:25 +0800

    feat: change

--- dependency.go
+++ dependency.go
@@ -1,11 +1,7 @@
 package go_mod_dependency_test
 
-import (
-	"fmt"
-	_ "github.com/gin-gonic/gin"
-	_ "github.com/wazsmwazsm/mortar"
-)
-
-func Dependency() {
-	fmt.Println("dependency : github.com/wazsmwazsm/mortar")
-}
+//
+//import (
+//	_ "github.com/gin-gonic/gin"
+//	_ "github.com/wazsmwazsm/mortar"
+//)
